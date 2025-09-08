6b64ae277a39d2ff0b58b6dda1cec88bf97f01d4
Arthur Vogel <arthur.vogel@xentral.com>
2025-09-08 13:32:18 +0200

    Fix url that was pointing to specific version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://developer.xentral.com/v25.33/docs/carrier-integration"
+            href="https://developer.xentral.com/docs/carrier-integration"
             target="_blank"
             rel="noopener noreferrer"
           >
