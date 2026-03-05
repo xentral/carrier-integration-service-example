b92cf4a8ebf03c4b61591dacf4b055ab419e1bef
Arthur Vogel <arthur.vogel@xentral.com>
2026-03-05 23:51:28 +0100

    Add the carrierOptions from the documentation examples to the service

--- src/app/carriers/route.ts
+++ src/app/carriers/route.ts
@@ -84,6 +84,66 @@ export async function GET(request: NextRequest) {
             shipmentType: [
               "outbound",
               "return"
+            ],
+            carrierOptions: [{
+              type: "select",
+              id: "AGE_CHECK",
+              name: "Age verification",
+              description: "When selected the age of the person receiving the parcel will be validated to be higher than the selected age",
+              required: true,
+              displayContext: "configuration",
+              availableValues: [
+                {
+                  id: "NONE",
+                  name: "No age check"
+                },
+                {
+                  id: "AGE_16",
+                  name: "16+"
+                },
+                {
+                  id: "AGE_18",
+                  name: "18+"
+                },
+                {
+                  id: "AGE_21",
+                  name: "21+"
+                },
+              ]
+            }, {
+              type: "select",
+              id: "PICKUP_TIME",
+              name: "Pickup time",
+              description: "Select the time that is most convenient for parcel pickup from the warehouse",
+              required: true,
+              displayContext: "both",
+              availableValues: [
+                {
+                  id: "MORNING",
+                  name: "08:00 - 11:00"
+                },
+                {
+                  id: "NOON",
+                  name: "11:00 - 13:00"
+                },
+                {
+                  id: "AFTERNOON",
+                  name: "13:00 - 17:00"
+                },
+                {
+                  id: "EVENING",
+                  name: "17:00 - 21:00"
+                },
+              ]
+            },
+            {
+              type: "number",
+              id: "PACKAGING_WEIGHT",
+              name: "Packaging Weight",
+              description: "The amount of weight in grams that should be added to the parcel weight to compensate for packing material.",
+              required: true,
+              displayContext: "labelPrint"
+            }
             ]
           }
         ]
